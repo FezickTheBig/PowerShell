ea51dfde26270607f99ec3ecaf2982d7bdd00d7f
Joshua Scott <Joshua.Scott@consolidated.com>
2019-05-18 01:33:47 -0700

    Cleaned up code and removed debugging statements

--- Replace.ps1
+++ Replace.ps1
@@ -1,24 +1,13 @@
 $count = 0
 $InputFiles = Get-Item "*.ini"
 $OldString  = 'Service=wdm disable=yes'
-$NewString  = ' '
-$InputFiles | ForEach {
-#    (Get-Content -Path $_.FullName).Replace($OldString,$NewString) | Set-Content -Path $_.FullName
+$NewString  = ''
+$InputFiles | ForEach-Object {
+
     if ((Get-Content -Path $_.FullName).Contains($OldString)) {
-#        Write-Host "Found IT!"
-#        $_
-#        $_.Replace($OldString,$NewString)
-#        Set-Content -Path $_.FullName
         (Get-Content -Path $_.FullName).Replace($OldString,$NewString) | Set-Content -Path $_.FullName
         $count++
     }
-
-<#
-  if ((Get-Content -Path $_.FullName).Contains($OldString)) {
-    $_.Replace($OldString,$NewString)
-    $count++
-  }
-#>
 
 }
 
